cd928226b3c1d83b183546fce7c121a89186a1eb
koomekevo <koome.kevo@gurucrafts.net>
2022-05-13 12:47:10 +0300

    Add email and password fields to users table

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -38,8 +38,6 @@ class Api::V1::UsersController < ApplicationController
     @user.destroy
   end
 
-  private
-
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
