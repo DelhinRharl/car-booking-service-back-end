8c910cb5ba620f17defd66bd99060c804bfaf71d
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-13 16:06:55 +0600

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :cars
   has_many :cars, through: :reservations
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
   def authenticate(provided_password)
     provided_password == password
