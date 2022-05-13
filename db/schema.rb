c336f8c3127a30872b96f8bd4b7b3ecaf2ec8d3a
koomekevo <koome.kevo@gurucrafts.net>
2022-05-13 12:50:39 +0300

    Run email and password db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_073913) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_094540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_073913) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
   end
 
   add_foreign_key "reservations", "cars"
