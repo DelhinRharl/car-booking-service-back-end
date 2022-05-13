96fd6df11088751d013f41205772c9854c1f6f38
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-13 16:03:27 +0600

    Add authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :cars
   has_many :cars, through: :reservations
+
+  def authenticate(provided_password)
+    provided_password == password
+  end
+  
 end
